29dc0388bb21f5ed2344ff7138d30e1c1464eccf
Agboola Akorede <akoredeagboola23@gmail.com>
2024-07-07 13:57:01 +0100

    reformat code

--- config/config.js
+++ config/config.js
@@ -1,50 +1,23 @@
 import { config } from 'dotenv';
 config();
 
-// export default {
-//   development: {
-//     username: 'postgres',
-//     password: process.env.MYSQL_PASSWORD,
-//     database: 'hng-stage2task',
-//     host: 'localhost',
-//     dialect: 'postgres',
-//   },
-//   test: {
-//     username: 'postgres',
-//     password: process.env.MYSQL_PASSWORD,
-//     database: 'hng-stage2task',
-//     host: 'localhost',
-//     dialect: 'postgres',
-//   },
-//   production: {
-//     username: 'postgres',
-//     password: process.env.MYSQL_PASSWORD,
-//     database: 'hng-stage2task',
-//     host: 'localhost',
-//     dialect: 'postgres',
-//   },
-// };
-
 export default {
   development: {
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
   },
   test: {
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
   },
   production: {
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
   },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 // jest.config.js
 export default {
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    "^.+\\.jsx?$": "babel-jest",
   },
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
-  moduleFileExtensions: ['js', 'ts', 'json', 'node'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts", ".tsx", ".jsx"],
+  moduleFileExtensions: ["js", "ts", "json", "node"],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  testTimeout: 200000,
 };
